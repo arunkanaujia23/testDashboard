3434ffc2b31ef45c55222d0ab015899f7352e82f
Arun <arunkanaujia23@gmail.com>
2024-08-02 00:08:17 +0530

    update

--- src/app/Components/Dashboard/Sidebar/Sidebar.jsx
+++ src/app/Components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,64 +1,105 @@
 'use client'
 import React from 'react'
 import styles from './sidebar.module.scss'
-import Logo from '@/assets/logo.png'
+import Logo from '@/assets/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
+// import ImagePath from '../../../../assets'
 import { usePathname } from 'next/navigation'
 
+const NavData = [
+  {
+    title: 'Overview',
+    route: '/',
+    icon: <svg fill="#ffffff" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="dashboard" class="icon glyph"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><rect x="2" y="2" width="9" height="11" rx="2"></rect><rect x="13" y="2" width="9" height="7" rx="2"></rect><rect x="2" y="15" width="9" height="7" rx="2"></rect><rect x="13" y="11" width="9" height="11" rx="2"></rect></g></svg>
+  },
+  {
+    title: 'Accounts',
+    route: '/',
+    icon: <svg width="20px" height="20px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M0 0h48v48H0z" fill="none"></path> <g id="Shopicon"> <path d="M33.843,26.914L24,36l-9.843-9.086C8.674,30.421,5,36.749,5,44h38C43,36.749,39.326,30.421,33.843,26.914z"></path> <path d="M24,28c3.55,0,6.729-1.55,8.926-4C34.831,21.876,36,19.078,36,16c0-6.627-5.373-12-12-12S12,9.373,12,16 c0,3.078,1.169,5.876,3.074,8C17.271,26.45,20.45,28,24,28z"></path> </g> </g></svg>
+  },
+  {
+    title: 'Transactions',
+    route: '/',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" class="bi bi-bank2" viewBox="0 0 16 16">
+      <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916zM12.375 6v7h-1.25V6zm-2.5 0v7h-1.25V6zm-2.5 0v7h-1.25V6zm-2.5 0v7h-1.25V6zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2M.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1z" />
+    </svg>
+  },
+  {
+    title: 'Reports',
+    route: '/',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="#ffffff" class="bi bi-file-earmark-pdf-fill" viewBox="0 0 16 16">
+      <path d="M5.523 12.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 6.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z" />
+      <path fill-rule="evenodd" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.7 11.7 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103" />
+    </svg>
+  },
+  {
+    title: 'Settings',
+    route: '/',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" class="bi bi-person-fill-gear" viewBox="0 0 16 16">
+      <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4m9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
+    </svg>
+  },
 
-const NavLinks = [`OverView`, `Accounts`, `Transactions`, `Reports`, `Settings `]
-
+]
 const Sidebar = () => {
-
 
   return (
     <div className={styles?.sidebarContainer}>
       <div className={styles?.sidebarLogo}>
-        <Image src={Logo} alt='Logo' fill />
+        <Image src={Logo} alt='Logo' height={120} />
       </div>
-    
+      <ul className={styles?.sidebarLinks}>
 
-      <div className={`${styles?.sidebarLinkContainer}`}>
-      
-        <ul className={styles?.sidebarLinks}>
-          {
-            NavLinks.map((link, index) => (
-              <li key={index} className={`${styles?.sidebarLink}`}>
-                <Link href={`#`}>{link}</Link>
+        {
+          NavData?.map((data, index) => {
+            return (
+              <li key={index} className={`${styles?.sidebarLink}  `}>
+                <Link href={data.route}>{data.icon} {data.title}</Link>
               </li>
-            ))
-          }
-      
+            )
+          })
+        }
+
       </ul>
+
+      <div className={`${styles?.sidebarLogoutButton}`}>
+        <Link href='#'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#333" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+          <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15zM11 2h.5a.5.5 0 0 1 .5.5V15h-1zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1" />
+        </svg> Logout</Link>
+
       </div>
     </div>
   )
 }
 
 const SidebarMobile = ({ closing }) => {
- 
+
   return (
     <div>
-      <div className={`${styles?.sidebarMobileContainer} ${closing && styles.active}` }>
+      <div className={`${styles?.sidebarMobileContainer} ${closing && styles.active}`}>
         <div className={styles?.sidebarMobileLogo}>
-          <Image src={Logo} alt='logo' fill/>
+          <Image src={Logo} alt='Logo' height={120} />
         </div>
-        
+        <ul className={styles?.sidebarMobileLinks}>
+          {
+             NavData?.map((data, index) => {
+              return (
+                <li key={index} className={`${styles?.sidebarMobileLink}  `}>
+                  <Link href={data.route}>{data.icon} {data.title}</Link>
+                </li>
+              )
+             })
+          }
+         
+        </ul>
 
-        <div className={`${styles?.sidebarLinkContainer}`}>
-      
-      <ul className={styles?.sidebarLinks}>
-        {
-          NavLinks.map((link, index) => (
-            <li key={index} className={`${styles?.sidebarLink}`}>
-              <Link href={`#`}>{link}</Link>
-            </li>
-          ))
-        }
-    
-    </ul>
-    </div>
+        <div className={`${styles?.sidebarMobileLogoutButton}`}>
+          <Link href='#'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#333" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+          <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15zM11 2h.5a.5.5 0 0 1 .5.5V15h-1zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1" />
+        </svg>Logout</Link>
+
+        </div>
       </div>
       <div className={styles?.bgSidebar} onClick={closing}>
 
